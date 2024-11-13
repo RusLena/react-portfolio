b1cf27c20aeb9f5885aaa281ec962479b377aafa
RusLena <myelvl@live.co.uk>
2024-11-13 23:15:27 +0000

    attempting to resolve btn click sound delay on Safari

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -29,24 +29,22 @@ const Header = () => {
     const clickSound = new Audio("./sounds/mixkit-fast-double-click-on-mouse-275.wav");
     clickSound.play();
   
-    // Check if the device is mobile (you can adjust the condition based on your needs)
+    // Check if the device is mobile
     const isMobile = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
-  
+  const isSafari = /Safari/i.test(navigator.userAgent) && !/Chrome/i.test(navigator.userAgent)
+  const delay = isMobile && isSafari ? 200 : 100;
     // Delay the link opening only for mobile devices
-    if (isMobile) {
+    
       setTimeout(() => {
         window.open("https://github.com/RusLena", "_blank");
-      }, 200);  // Delay for mobile devices to ensure smooth sound play
-    } else {
-      // For desktop, open the link immediately
-      window.open("https://github.com/RusLena", "_blank");
-    }
+      }, delay);  
   };
+  
   return (
     <header className="header">
-      <Navbar /> {/* Render Navbar here */}
+      <Navbar /> 
       <section id="about" className="about-section">
-        {/* Framer Motion for Heading Animation */}
+    
         <motion.h1
           className={`heading ${isGlowing ? "glow" : ""}`}
           initial={{ x: "-100%", opacity: 0 }}
@@ -61,14 +59,12 @@ const Header = () => {
           I'm a Freelance Frontend Developer.
         </motion.h1>
 
-        {/* Static image, no animation */}
         <img
           className="photo"
           src="/images/Portfolio-photo.png"
           alt="me in london"
         />
 
-        {/* React Spring for Intro Text animation */}
         <animated.div className="intro" style={slideInProps}>
           <p>
             I gained foundational knowledge in Front-End Web Development through
